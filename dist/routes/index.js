7f4b7f83cdd878d6049e1d704765aca57388376a
Robert Greenawalt <robert.greenawalt29@gmail.com>
2022-08-17 23:56:19 -0400

    edit path to client build folder for production for routes

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -10,7 +10,14 @@ const api_1 = __importDefault(require("./api"));
 router.use("/api", api_1.default);
 // If no API routes are hit, send the React app
 router.use(function (req, res) {
-    res.sendFile((0, path_1.join)(__dirname, "../client/build/index.html"));
+    if (process.env.NODE_ENV === 'production') {
+        // in production, we're already in app/dist/routes/index.js. We need to go outside of app/dist/routes/index.js all the way back into app/ and then from there into client/
+        // this will be in /app/dist/client/build/index.html
+        res.sendFile((0, path_1.join)(__dirname, "../../client/build/index.html"));
+    }
+    else {
+        res.sendFile((0, path_1.join)(__dirname, "../client/build/index.html"));
+    }
 });
 exports.default = router;
 //# sourceMappingURL=index.js.map
--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,22 @@
+import { Request, Response } from "express";
+import { join } from "path";
+
 const router = require("express").Router();
 import apiRoutes from "./api";
 
 // API routes
 router.use("/api", apiRoutes);
 
-
+// If no API routes are hit, send the React app
+router.use(function (req: Request, res: Response) {
+    if (process.env.NODE_ENV === 'production') {
+        // in production, we're already in app/dist/routes/index.js. We need to go outside of app/dist/routes/index.js all the way back into app/ and then from there into client/
+        // this will be in /app/dist/client/build/index.html
+        res.sendFile(join(__dirname, "../../client/build/index.html"));
+    } else {
+        res.sendFile(join(__dirname, "../client/build/index.html"));
+    }
+});
 
 
 export default router;
