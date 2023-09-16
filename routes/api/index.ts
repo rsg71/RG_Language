27f5deac1ff29ed860b6c2521d0bf15bed706c22
Robert Greenawalt <robert.greenawalt29@gmail.com>
2023-09-15 21:57:29 -0400

    Display time in healthcheck endpoint

--- routes/api/index.ts
+++ routes/api/index.ts
@@ -12,7 +12,8 @@ router.use("/auth", authRoutes);
 
 router.get("/status", (req: Request, res: Response) => {
     const date = new Date().toLocaleDateString();
-    res.status(200).send(`Status: API is working as of ${date}`);
+    const time = new Date().toLocaleTimeString();
+    res.status(200).send(`Status: API is working as of ${date} at ${time}`);
 })
 
 // User routes 
